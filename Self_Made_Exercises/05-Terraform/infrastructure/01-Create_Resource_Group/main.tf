5daa52cdd2779d6d5fa2cad24b2e9e8fbab89dd2
Erivan Buenaventura <109859566+evan-07@users.noreply.github.com>
2022-10-24 14:08:35 +0800

    updated main.tf in RG creation

--- Self_Made_Exercises/05-Terraform/infrastructure/01-Create_Resource_Group/main.tf
+++ Self_Made_Exercises/05-Terraform/infrastructure/01-Create_Resource_Group/main.tf
@@ -14,17 +14,17 @@ terraform {
 # our Azure account
 
 provider "azurerm" {
-  subscription_id = "6912d7a0-bc28-459a-9407-33bbba641c07"
-  client_id       = "230411ec-45e9-4650-95b2-7675131e2d1a"
-  client_secret   = "8D~7Q~y39tBTXsFXGcuVIwvGCOorRUo6dXtwX"
-  tenant_id       = "70c0f6d9-7f3b-4425-a6b6-09b47643ec58"
+  subscription_id = ""
+  client_id       = ""
+  client_secret   = ""
+  tenant_id       = ""
   features {}
 }
 
 # The resource block defines the type of resource we want to work with
 # The name and location are arguements for the resource block
 
-resource "azurerm_resource_group" "app_grp"{
-  name="app-grp" 
-  location="North Europe"
-}
+resource "azurerm_resource_group" "rg"{
+  name="rg" 
+  location="East US 2"
+}s
