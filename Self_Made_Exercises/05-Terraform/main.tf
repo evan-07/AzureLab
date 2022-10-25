af3dbadb78adecb12e714bc83c4376329c881941
erivan <erivanbuenaventura@gmail.com>
2022-10-25 17:10:40 +0000

    updated main.tf

--- Self_Made_Exercises/05-Terraform/main.tf
+++ Self_Made_Exercises/05-Terraform/main.tf
@@ -2,7 +2,7 @@
 terraform {
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm" 
+      source  = "hashicorp/azurerm"
       version = "=3.0.0"
     }
   }
@@ -10,10 +10,6 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "2b533d5a-4f2a-4434-955c-48eb697123c6"
-  client_id       = "9cde2962-8b28-4657-afd0-f10d8de0e1c5"
-  client_secret   = "YbN8Q~h7~gjgjrpqtc8thD4f9jkrZOyAplZE5aVB"
-  tenant_id       = "ebc1b0d9-c149-4b7f-ae19-45d09a40413b"
 }
 
 resource "azurerm_resource_group" "rg" {
